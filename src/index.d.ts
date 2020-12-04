e81570492192b1abf0fd08161473aa84edf34e2a
zyxd <zyxd@syncraft.ru>
2020-12-04 21:03:22 +0400

    Added typescript declaration

---
+++ src/index.d.ts
@@ -0,0 +1,16 @@
+interface MatchProps {
+  path: string
+  pattern?: string
+}
+
+declare module 'svelte-store-router' {
+  export function createRouteStore(options: any): string
+
+  export class Match {
+		$$prop_def: MatchProps
+  }
+  
+  export class Matcher {
+    $$prop_def: any
+  }
+}
